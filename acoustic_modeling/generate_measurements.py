b83fc021d3c02cf690ac05a30244989569fef20d
ljlozenski <ljlozenski@wustl.edu>
2025-03-20 19:43:06 -0500

    updates for Jenil

--- acoustic_modeling/generate_measurements.py
+++ acoustic_modeling/generate_measurements.py
@@ -6,6 +6,10 @@ import scipy.interpolate as interp
 import scipy.sparse as sp
 import time
 
+from pylops import MatrixMult
+from pylops.signalprocessing import Convolve1D, Convolve2D
+from pylops.optimization.leastsquares import regularized_inversion
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,26 +27,30 @@ import argparse
 if __name__ == "__main__":
     
     imaging_op_folder = '/workspace/shared_data/msot_imaging_op/'
-    p0_folder =  '/workspace/shared_data/p0_multi_wave/'
+    p0_folder =  '/workspace/shared_data/Moby_multi_wave/'
 
-    Nx = 332
-    nx = 208
-    nm0 = 1249
-    nm1 = 256
-    vals = np.squeeze(io.loadmat(imaging_op_folder + 'imaging_vals.mat')['V'])
-    ir = np.squeeze(io.loadmat(imaging_op_folder + 'imaging_rows.mat')['I']-1)
-    jc = np.squeeze(io.loadmat(imaging_op_folder + 'imaging_cols.mat')['J']-1)
-    a = sp.csc_matrix((vals, (ir, jc)), shape=(nm0*nm1, Nx**2))
+    Nx = 248
+    nx = 248
+    
+    ndet = 256
+    ntm = 2030
+    
+    a = sp.load_npz("imaging_op.npz")
+    imp_resp = np.load("imp_resp.npy")
+    convolve_irf = Convolve1D((ndet, ntm), imp_resp, axis=1, offset=ntm // 2,
+                                      dtype=np.float32)
+
+    imaging_op = convolve_irf @ MatrixMult(a)
 
     lambs = [730,  750,  770,  780,  790,  800,  820,  840]
     nl = len(lambs)
 
-    ssf = 6
+    ssf = 1
 
     nT = 9600
     nt = nT//(ssf*nl)
 
-    measurements = np.zeros((nm0,nm1, nl, nt))
+    measurements = np.zeros((ndet, ntm, nl, nt))
 
 
     F = np.zeros((Nx,Nx,ssf))
@@ -53,15 +61,22 @@ if __name__ == "__main__":
         for l in range(nl):
             for j in range(ssf):
                 
-                fid = l + j*ssf + t*ssf*nl + 1
+                fid = l + j*ssf + t*ssf*nl
 
-                fname = p0_folder + 'p0_' + str(lambs[l]) + '/p0_' + str(fid) + '.mat'
+                fname = p0_folder + 'p0_' + str(lambs[l]) + '/moby_' + str(fid) + '.mat'
                 f = io.loadmat(fname)['p0']
-                fi = interp.interp2d(x,x, f)
-                F[:,:,j] = fi(X,X)
+                #fi = interp.interp2d(x,x, f)
+                #F[:,:,j] = fi(X,X)
+                F[:,:,j] = np.mean(f,-1)
+
+                #measurements[:,:,l,t] += imaging_op.matvec(F[:,:,j].flatten()).reshape((ndet,ntm))/ssf
+                
+                
            
-            M = a@F.reshape((Nx**2,ssf),order = 'F')
-            measurements[:,:,l,t] = np.mean(M,-1).reshape((nm0,nm1), order = 'F')
+            M = imaging_op.matmat(np.flip(F,0).reshape((Nx**2,ssf)))
+            measurements[:,:,l,t] = np.mean(M,-1).reshape((ndet,ntm))
+
+    np.save('acoustic_measurements.npy', measurements)
     io.savemat('acoustic_measurements.mat',{'measurements': measurements})
 
 
--- acoustic_modeling/subsample_p0.py
+++ acoustic_modeling/subsample_p0.py
@@ -22,16 +22,16 @@ import argparse
 
 if __name__ == "__main__":
     
-    p0_folder =  '/workspace/shared_data/p0_multi_wave/'
+    p0_folder =  '/workspace/shared_data/Moby_multi_wave/'
 
-    Nx = 332
-    nx = 208
+    Nx = 248
+    nx = 248
 
 
     lambs = [730,  750,  770,  780,  790,  800,  820,  840]
     nl = len(lambs)
 
-    ssf = 6
+    ssf = 1
 
     nT = 9600
     nt = nT//(ssf*nl)
@@ -46,13 +46,22 @@ if __name__ == "__main__":
     for t in range(nt):
         print(t)
         for l in range(nl):
-            for j in range(nl*ssf):
-                fid = t*nl*ssf + j + 1
-                fname = p0_folder + 'p0_' + str(lambs[l]) + '/p0_' + str(fid) + '.mat'
-                f = io.loadmat(fname)['p0']
-                fi = interp.interp2d(x,x, f)
-                F[:,:,l,t] += fi(X,X)/(nl*ssf)    
-    io.savemat('subsample_p0.mat',{'p0': F})
+            for j in range(ssf):
+                fid = l + j*ssf + t*ssf*nl
+                fname = p0_folder + 'p0_' + str(lambs[l]) + '/moby_' + str(fid) + '.mat'
+                f = io.loadmat(fname)['p0'] 
+                F[:,:,l,t] += np.mean(f,-1)/(ssf) 
+    """try:
+        print("Saving as 1 file")
+        io.savemat('subsample_p0.mat',{'p0': F})"""
+        
+    """except:
+        print("Saving as 3 files")"""
+    io.savemat('subsample_p00.mat',{'p0': F[:,:,:,::3]}, do_compression = True)
+    io.savemat('subsample_p01.mat',{'p0': F[:,:,:,1::3]}, do_compression = True)
+    io.savemat('subsample_p02.mat',{'p0': F[:,:,:,2::3]}, do_compression = True)
+        
+        
 
 
 
